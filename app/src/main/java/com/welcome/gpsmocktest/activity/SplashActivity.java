479c37a4c1313e70eece8d6715b39ab1ff4fb54d
wuxiaoqi <1321972760@qq.com>
2020-02-17 14:12:50 +0800

    splash

---
+++ app/src/main/java/com/welcome/gpsmocktest/activity/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.welcome.gpsmocktest.activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+import com.welcome.gpsmocktest.MainActivity;
+import com.welcome.gpsmocktest.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplashActivity extends BaseActivity {
+
+    private static final int REQUEST_PERMISSION_CODE = 1;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initView() {
+        Button startBtn = findViewById(R.id.startButton);
+        getPermissions();
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMain();
+            }
+        });
+    }
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            List<String> permissions = new ArrayList<>();
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            int size = permissions.size();
+            if (size > 0) {
+                requestPermissions(permissions.toArray(new String[size]), REQUEST_PERMISSION_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void startMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
