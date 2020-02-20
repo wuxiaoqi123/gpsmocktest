6703599e4cfe39c24fc0bff16e84504ab9080218
wuxiaoqi <1321972760@qq.com>
2020-02-20 10:02:08 +0800

    gps

--- app/src/main/java/com/welcome/gpsmocktest/MainActivity.java
+++ app/src/main/java/com/welcome/gpsmocktest/MainActivity.java
@@ -2,21 +2,36 @@ package com.welcome.gpsmocktest;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.NetworkUtil;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.google.android.material.navigation.NavigationView;
 import com.welcome.gpsmocktest.activity.BaseActivity;
 import com.welcome.gpsmocktest.db.HistoryDBHelper;
@@ -28,6 +43,7 @@ import org.apache.log4j.Logger;
 
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int ACTION_LOCATION_SOURCE_SETTINGS_CODE = 1;
     private static Logger log = Logger.getLogger(MainActivity.class);
 
     private HistoryDBHelper historyDBHelper;
@@ -42,6 +58,22 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private BaiduMap mBaiduMap = null;
     private boolean isNetworkConnected = true;
     private boolean isGPSOpen = false;
+    private boolean isFirstLoc = true;
+
+    private LocationClient mLocClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+    private MyLocationConfiguration.LocationMode mCurrentMode;
+    private BitmapDescriptor mCurrentMarker;
+    private SensorManager mSensorManager;
+    private double lastX = 0.0;
+    private int mCurrentDirection = 0;
+    private double mCurrentLat = 0.0;
+    private double mCurrentLon = 0.0;
+    private float mCurrentAccracy;
+    private String mCurrentCity = "";
+    private String mCurrentAddr;
+    private MyLocationData locData;
+
 
     @Override
     protected int getLayoutId() {
@@ -94,10 +126,53 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
         mMapView = findViewById(R.id.mapView);
         mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!isGPSOpen) {
+            showGpsDialog();
+        } else {
+            openLocateLayer();
+        }
     }
 
     private void initListener() {
 
+    }
+
+    private void showGpsDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Tips")
+                .setMessage("是否开启GPS定位服务?")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(intent, ACTION_LOCATION_SOURCE_SETTINGS_CODE);
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .show();
+    }
+
+    private void openLocateLayer() {
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setIsNeedAddress(true);
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTION_LOCATION_SOURCE_SETTINGS_CODE) {
+            if (isGPSOpen = isGpsOpened()) {
+                openLocateLayer();
+            }
+        }
     }
 
     @Override
@@ -125,6 +200,35 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
                 } else if (statusCode == MockGpsService.StopCode) {
                     isServiceRun = false;
                 }
+            }
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation == null || mMapView == null) {
+                return;
+            }
+            mCurrentAddr = bdLocation.getAddrStr();
+            mCurrentCity = bdLocation.getCity();
+            mCurrentLat = bdLocation.getLatitude();
+            mCurrentLon = bdLocation.getLongitude();
+            mCurrentAccracy = bdLocation.getRadius();
+            locData = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    .direction(mCurrentDirection)
+                    .latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(bdLocation.getLatitude(),
+                        bdLocation.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
             }
         }
     }
