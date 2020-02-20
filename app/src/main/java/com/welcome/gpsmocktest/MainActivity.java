c912edd68f9a4510706261dc8afe588c0eb7902b
wuxiaoqi <1321972760@qq.com>
2020-02-20 10:47:49 +0800

    map

--- app/src/main/java/com/welcome/gpsmocktest/MainActivity.java
+++ app/src/main/java/com/welcome/gpsmocktest/MainActivity.java
@@ -7,15 +7,23 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.sqlite.SQLiteDatabase;
 import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -32,6 +40,8 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.poi.PoiSearch;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
 import com.google.android.material.navigation.NavigationView;
 import com.welcome.gpsmocktest.activity.BaseActivity;
 import com.welcome.gpsmocktest.db.HistoryDBHelper;
@@ -59,6 +69,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private boolean isNetworkConnected = true;
     private boolean isGPSOpen = false;
     private boolean isFirstLoc = true;
+    private boolean isMockLocOpen = false;
 
     private LocationClient mLocClient = null;
     private MyLocationListener myListener = new MyLocationListener();
@@ -74,6 +85,17 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private String mCurrentAddr;
     private MyLocationData locData;
 
+    private PoiSearch poiSearch;
+    private SearchView searchView;
+    private ListView searchList;
+    private ListView historySearchList;
+    private SimpleAdapter simpleAdapter;
+    private LinearLayout mLinearLayout;
+    private LinearLayout mHistoryLinearLayout;
+    private MenuItem searchItem;
+    private boolean isSubmit;
+    private SuggestionSearch mSuggestionSearch;
+
 
     @Override
     protected int getLayoutId() {
@@ -82,6 +104,8 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     @Override
     public void initView() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
         LogUtil.configLog();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -92,9 +116,13 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
                 drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        searchView = findViewById(R.id.action_search);
+        searchList = findViewById(R.id.search_list_view);
+        mLinearLayout = findViewById(R.id.search_linear);
+        historySearchList = findViewById(R.id.search_history_list_view);
+        mHistoryLinearLayout = findViewById(R.id.search_history_linear);
 
         registerMockReceiver();
         initBaiduMap();
@@ -132,6 +160,16 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         } else {
             openLocateLayer();
         }
+        poiSearch = PoiSearch.newInstance();
+        isMockLocOpen = isAllowMockLocation();
+        if (!isMockLocOpen) {
+            showOpenMockDialog();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(getApplicationContext())) {
+                showFloatWindowDialog();
+            }
+        }
     }
 
     private void initListener() {
@@ -163,6 +201,54 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         option.setScanSpan(1000);
         mLocClient.setLocOption(option);
         mLocClient.start();
+    }
+
+    private void showOpenMockDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("启用位置模拟")
+                .setMessage("请在\"开发者选项→选择模拟位置信息应用\"中进行设置")
+                .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            displayToast("无法跳转到开发者选项,请先确保您的设备已处于开发者模式");
+                        }
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .show();
+    }
+
+    private void showFloatWindowDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("启用悬浮窗")
+                .setMessage("为了模拟定位的稳定性，建议开启\"显示悬浮窗\"选项")
+                .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            displayToast("无法跳转到设置界面，请在权限管理中开启该应用的悬浮窗");
+                        }
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .show();
+    }
+
+    public void setTraffic(View view) {
+        mBaiduMap.setTrafficEnabled(((CheckBox) view).isChecked());
+    }
+
+    public void setBaiduHeatMap(View view) {
+        mBaiduMap.setBaiduHeatMapEnabled(((CheckBox) view).isChecked());
     }
 
     @Override
--- app/src/main/java/com/welcome/gpsmocktest/activity/BaseActivity.java
+++ app/src/main/java/com/welcome/gpsmocktest/activity/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.welcome.gpsmocktest.activity;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
 
@@ -11,6 +16,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    public static final String TAG = "GpsMockTest";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,5 +41,77 @@ public abstract class BaseActivity extends AppCompatActivity {
     public boolean isGpsOpened() {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    //模拟位置权限开启
+    public boolean isAllowMockLocation() {
+        boolean canMockPosition;
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            canMockPosition = Settings.Secure.getInt(getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION, 0) != 0;
+        } else {
+            try {
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                String providerStr = LocationManager.GPS_PROVIDER;
+                LocationProvider provider = locationManager.getProvider(providerStr);
+                try {
+                    locationManager.removeTestProvider(providerStr);
+                    Log.d(TAG, "try to move test provider");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "try to move test provider");
+                }
+                if (provider != null) {
+                    try {
+                        locationManager.addTestProvider(
+                                provider.getName(),
+                                provider.requiresNetwork(),
+                                provider.requiresSatellite(),
+                                provider.requiresCell(),
+                                provider.hasMonetaryCost(),
+                                provider.supportsAltitude(),
+                                provider.supportsSpeed(),
+                                provider.supportsBearing(),
+                                provider.getPowerRequirement(),
+                                provider.getAccuracy()
+                        );
+                        canMockPosition = true;
+                    } catch (Exception e) {
+                        Log.e(TAG, "add origin gps test provider error");
+                        canMockPosition = false;
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        locationManager.addTestProvider(
+                                providerStr,
+                                true,
+                                true,
+                                false,
+                                false,
+                                true,
+                                true,
+                                true,
+                                Criteria.POWER_HIGH,
+                                Criteria.ACCURACY_FINE
+                        );
+                        canMockPosition = true;
+                    } catch (Exception e) {
+                        Log.e(TAG, "add gps test provider error");
+                        canMockPosition = false;
+                        e.printStackTrace();
+                    }
+                }
+                if (canMockPosition) {
+                    locationManager.setTestProviderEnabled(providerStr, true);
+                    locationManager.setTestProviderStatus(providerStr, LocationProvider.AVAILABLE, null, System.currentTimeMillis());
+                    locationManager.setTestProviderEnabled(providerStr, false);
+                    locationManager.removeTestProvider(providerStr);
+                }
+            } catch (Exception e) {
+                canMockPosition = false;
+                e.printStackTrace();
+            }
+        }
+        return canMockPosition;
     }
 }
